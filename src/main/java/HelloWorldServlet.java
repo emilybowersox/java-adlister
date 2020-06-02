3cc75356709160418c890e6ac66e3c1df7ab2dd0
Emily Bowersox <emily.bowersox100@gmail.com>
2020-06-02 13:55:16 -0500

    created hello world servlet class

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,9 +1,19 @@
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
+@WebServlet (name = "HelloWorldServlet", urlPatterns = "/hello-world")
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) {
-
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+                res.setContentType("text/html");
+                PrintWriter out = res.getWriter();
+                out.println("<h6>Hello, Emily!</h6>");
     }
 }
+
+
+//have to restart server the first time to the launch the page, after that use "redeploy" to recompile(?)
