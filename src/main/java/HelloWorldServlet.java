0f0fe739b4864260877641a7a6505330b53c1bbd
Emily Bowersox <emily.bowersox100@gmail.com>
2020-06-02 15:15:58 -0500

    added if statement to add parameter for name

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -5,13 +5,19 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet (name = "HelloWorldServlet", urlPatterns = "/hello-world")
+@WebServlet (name = "HelloWorldServlet", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
                 res.setContentType("text/html");
                 PrintWriter out = res.getWriter();
-                out.println("<h6>Hello, Emily!</h6>");
+        String name = req.getParameter("name");
+
+        if(name == null){
+            out.println("<h6>Hello world!</h6>");;
+        }else {
+            out.println("<h6>Hello " + name + "!</h6>");
+        }
     }
 }
 
