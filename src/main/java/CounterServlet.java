4d8f3a60745cfeef41ec11aa2a282f7ec56a7805
Emily Bowersox <emily.bowersox100@gmail.com>
2020-06-02 15:22:19 -0500

    counter bonus exercise

---
+++ src/main/java/CounterServlet.java
@@ -0,0 +1,23 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet (name = "CounterServlet", urlPatterns = "/count")
+public class CounterServlet extends HttpServlet {
+
+    int counter = 0;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        counter++;
+        res.setContentType("text/html");
+        PrintWriter out = res.getWriter();
+        out.println("Counter: " + counter);
+
+            }
+        }
+
