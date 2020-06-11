47b7b7ef8692c1d6902e8fbd96558849e8d94fda
Emily Bowersox <emily.bowersox100@gmail.com>
2020-06-11 12:03:26 -0500

    created db with users and ads tables

---
+++ ad_migration.sql
@@ -0,0 +1,25 @@
+
+CREATE DATABASE IF NOT EXISTS adlister_db;
+
+USE adlister_db;
+
+DROP TABLE IF EXISTS ads;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE ads (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    user_id INT UNSIGNED NOT NULL,
+    title VARCHAR(200) NOT NULL,
+    description TEXT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users (id)
+                 on DELETE CASCADE
+);
